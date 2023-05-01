25ba9197672553cd470477afeef1b15cdba5201a
AaronTweeton <aaron@aarontweeton.com>
2023-05-01 04:31:32 -0700

    Created admin page.

--- fantastic-packages.php
+++ fantastic-packages.php
@@ -31,17 +31,34 @@ if (!defined('ABSPATH')) {
     return;
 }
 
+
 if (!class_exists('FantasticPackages_Plugin')) {
     class FantasticPackages_Plugin {
         static $instance = false;
 
         private function __construct() {
+            add_action('admin_menu', array($this, 'create_admin_page'));
         }
 
         public static function get_instance(): FantasticPackages_Plugin {
             if (!self::$instance)
                 self::$instance = new self;
             return self::$instance;
+        }
+
+        public function compose_menu_page() {
+            require_once plugin_dir_path(__FILE__) . 'includes/admin-page.php';
+        }
+
+        public function create_admin_page(): void {
+            add_menu_page(
+                'Fantastic Packages Demo',
+                'Fantastic Packages',
+                'manage_options',
+                'fantastic-packages',
+                array($this, 'compose_menu_page'),
+                'dashicons-drumstick',
+            );
         }
     }
 }
