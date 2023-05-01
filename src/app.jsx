4cb23f0a98e04951546826079888c7849463b6ae
AaronTweeton <aaron@aarontweeton.com>
2023-05-01 06:14:06 -0700

    Importing @wordpress/date. Converted list to table. Using date function to convert date.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import apiFetch from "@wordpress/api-fetch";
 import { useEffect, useState } from "@wordpress/element";
 import { Button, Flex, Spinner } from "@wordpress/components";
+import { date } from "@wordpress/date";
 
 export function App() {
   const [posts, setPosts] = useState([]);
@@ -19,18 +20,34 @@ export function App() {
     <div>
       <h2>Latest posts</h2>
       {posts.length ? (
-        <ul>
-          {posts.map((post) => (
-            <li>
-              <Flex justify="start">
-                {post.title.rendered}
-                <Button href={post.link} variant="secondary">
-                  View
-                </Button>
-              </Flex>
-            </li>
-          ))}
-        </ul>
+        <table className="widefat fixed striped">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <>
+              {posts.map((post) => (
+                <tr>
+                  <td class="row-title">{post.title.rendered}</td>
+                  <td class="date description">
+                    {date("F j, Y, g:i a", post.date)}
+                  </td>
+                  <td>
+                    <Flex justify="end">
+                      <Button href={post.link} variant="secondary">
+                        View
+                      </Button>
+                    </Flex>
+                  </td>
+                </tr>
+              ))}
+            </>
+          </tbody>
+        </table>
       ) : (
         <>
           Fetching posts <Spinner />
